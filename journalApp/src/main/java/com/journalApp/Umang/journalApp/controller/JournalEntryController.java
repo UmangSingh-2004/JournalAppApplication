eb093456a5faeb89d5d10ba2b5041fb9731cfd4f
Umang Singh <umang8933@gmail.com>
2026-08-12 23:33:21 +0530

    GetMapping Endpoint

--- journalApp/src/main/java/com/journalApp/Umang/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/com/journalApp/Umang/journalApp/controller/JournalEntryController.java
@@ -1,8 +1,22 @@
 package com.journalApp.Umang.journalApp.controller;
 
+import com.journalApp.Umang.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntryMap = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntryMap.values());
+    }
 
 }
