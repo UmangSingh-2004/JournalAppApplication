ee5aeb42ad5f072fc5ffa476a5f10a2e7ec03c68
Umang Singh <umang8933@gmail.com>
2026-08-12 22:40:26 +0530

    New End points

--- journalApp/src/main/java/com/journalApp/Umang/journalApp/controller/HealthCheck.java
+++ journalApp/src/main/java/com/journalApp/Umang/journalApp/controller/HealthCheck.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
-    @GetMapping("/HealthCheck")
+    @GetMapping("/Health-Check")
     public String healthCheck(){
         return "Ok";
     }
