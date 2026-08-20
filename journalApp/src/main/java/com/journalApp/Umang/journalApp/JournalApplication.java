218e1b993592d7a3e0f2fad36cf7c7452e5df62e
Umang Singh <umang8933@gmail.com>
2026-08-20 23:09:23 +0530

    @PutMapping and @DeleteMapping

--- journalApp/src/main/java/com/journalApp/Umang/journalApp/JournalApplication.java
+++ journalApp/src/main/java/com/journalApp/Umang/journalApp/JournalApplication.java
@@ -8,5 +8,6 @@ public class JournalApplication {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(JournalApplication.class, args);
+		System.out.println("Journal Application Started");
 	}
 }
